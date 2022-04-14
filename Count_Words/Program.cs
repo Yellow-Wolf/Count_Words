4f341fcce4db10c0a73378f5d1bcc9859742f6e6
YanishYa <86955491+YanishYa@users.noreply.github.com>
2022-04-14 16:44:54 +0300

    Update Program.cs

--- Count_Words/Program.cs
+++ Count_Words/Program.cs
@@ -8,6 +8,18 @@ namespace Count_Words
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("test World!");
+            int count = 0;
+            list<string> lst = new list<string>();
+            string l = Console.ReadLine();
+            while (l != " "){
+                for (int i = 0; i < lst.Count; i++){
+                        if (lst[i] == l){
+                            break();
+                        }
+                }
+                lst.Add(l);
+                l = Console.ReadLine();
+            }
         }
     }
 }
