a2b26869eced65bbf806619cd656dfd88b249124
holliwid <53123980+holliwid@users.noreply.github.com>
2022-05-18 22:31:39 +0300

    add class BoxPlot

---
+++ BoxPlot.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_Words
+{
+    internal class BoxPlot
+    {
+        myWordCount mc = new myWordCount();
+        public int median()
+        {
+            mc.WordCount();
+            var k = mc.wordList.Values.ToArray();
+            int sum = k.Sum();
+
+            var accum = 0;
+            for (int i = 0; i < k.Length; i++)
+            {
+                accum += k[i];
+                if (accum >= sum / 2)
+                    return i;
+            }
+
+            return k.Length;
+        }
+    }
+}
